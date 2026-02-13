f27bb0352830775150321ae8b583cb125632e742
Liliia Fodorova <nevskay22@gmail.com>
2026-02-13 23:16:05 +0100

    add file js, html, joson

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '54644062-931f8757c3983fa8d6dda8238';
+
+export function getImagesByQuery(query) {
+    console.log(query); 
+    // delete
+    return axios.get(BASE_URL, {
+        params: {
+            key:API_KEY,
+            q: query,
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+        },
+    })
+        .then(response => {
+            console.log(responss.data);
+            // delete
+            return response.data;
+        });
+}
---
+++ src/js/render-functions.js
@@ -0,0 +1,48 @@
+import SimpleLightbox from 'simpleLightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryEl = document.querySelector('.gallery');
+const loaderEl = document.querySelector('.loader');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
+
+export function createGallery(images) {
+    // delete
+    console.log(images);
+    console.log(images.length);
+
+    const markup = images.map(image =>
+        <li class="galery-item">
+            <a href="${image.largeImageURL}">
+                <img src="${image.webformatURL}"
+                    alt="${image.tags}"
+                    loading="lazy"
+                />
+            </a>
+            <div class="info">
+                <p><strong>Likes</strong> ${image.likes}</p>
+                <p><strong>Views</strong> ${image.views}</p>
+                <p><strong>Comments</strong> ${image.comments}</p>
+                <p><strong>Downloads</strong> ${image.downloads}</p>
+            </div>
+        </li>
+        
+    ).join('');
+    gaalleryEl.insertAdjacentHTML('beforeend', markup);
+    lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryEl.innerHTML = '';
+}
+
+export function showLoader() {
+  loaderEl.classList.remove('is-hidden');
+}
+
+export function hideLoader() {
+  loaderEl.classList.add('is-hidden');
+}
